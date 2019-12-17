ffad2c0fa6f82f4812c8a4baf6fcc71dc7afe14d
binhnguyen <binhnd@socicom.com.vn>
2019-12-17 11:57:04 +0700

    Remove onLsMessage handler

--- src/WebView.android.tsx
+++ src/WebView.android.tsx
@@ -222,13 +222,6 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     }
   };
 
-  onLsMessage = (event: WebViewMessageEvent) => {
-    const { onLsMessage } = this.props;
-    if (onLsMessage) {
-      onLsMessage(event);
-    }
-  };
-
   onLoadingProgress = (event: WebViewProgressEvent) => {
     const { onLoadProgress } = this.props;
     const { nativeEvent: { progress } } = event;
@@ -283,7 +276,6 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
   render() {
     const {
       onMessage,
-      onLsMessage,
       onShouldStartLoadWithRequest: onShouldStartLoadWithRequestProp,
       originWhitelist,
       renderError,
@@ -345,7 +337,6 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
         onLoadingStart={this.onLoadingStart}
         onHttpError={this.onHttpError}
         onMessage={this.onMessage}
-        onLsMessage={this.onLsMessage}
         onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
         onShouldCreateNewWindow={this.onCreateNewWindow}
         onNavigationStateChange={this.updateNavigationState}
