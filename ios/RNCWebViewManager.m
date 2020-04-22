a8002c82e161e4433aea228a9256a110a4a043c0
BinhND <binhnd@socicom.com.vn>
2020-04-22 17:49:56 +0700

    [iOS] Update on new window

--- ios/RNCWebViewManager.m
+++ ios/RNCWebViewManager.m
@@ -91,6 +91,7 @@ RCT_EXPORT_VIEW_PROPERTY(onNavigationStateChange, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(messagingEnabled, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(onMessage, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onScroll, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onWebviewClosed, RCTDirectEventBlock)
 
 RCT_EXPORT_VIEW_PROPERTY(contentRuleLists, NSArray<NSString>)
 
@@ -333,35 +334,32 @@ RCT_EXPORT_METHOD(startLoadWithResult:(BOOL)result lockIdentifier:(NSInteger)loc
   request[@"lockIdentifier"] = @(createNewWindowCondition.condition);
   callback(request);
   
-  // // Block the main thread for a maximum of 250ms until the JS thread returns
-  // if ([createNewWindowCondition lockWhenCondition:0 beforeDate:[NSDate dateWithTimeIntervalSinceNow:.25]]) {
-  //   [createNewWindowCondition unlock];
-  //   createNewWindowCondition = nil;
-  //   if (createNewWindowResult) {
-  //     newWindow = [RNCWebView new];
-  //     [newWindow setupConfiguration:configuration];
-  //     return newWindow;
-  //   } else {
-  //     return nil;
-  //   }
-  // } else {
-  //   RCTLogWarn(@"Did not receive response to shouldCreateNewWindow in time, defaulting to YES");
-  //   newWindow = [RNCWebView new];
-  //   [newWindow setupConfiguration:configuration];
-  //   return newWindow;
-  // }
-  return nil;
+  // Block the main thread for a maximum of 250ms until the JS thread returns
+  if ([createNewWindowCondition lockWhenCondition:0 beforeDate:[NSDate dateWithTimeIntervalSinceNow:.25]]) {
+    [createNewWindowCondition unlock];
+    createNewWindowCondition = nil;
+    if (createNewWindowResult) {
+      newWindow = [[RNCWebView alloc] initWithConfiguration:configuration];
+      return newWindow;
+    } else {
+      return nil;
+    }
+  } else {
+    RCTLogWarn(@"Did not receive response to shouldCreateNewWindow in time, defaulting to YES");
+    newWindow = [[RNCWebView alloc] initWithConfiguration:configuration];
+    return newWindow;
+  }
 }
 
 RCT_EXPORT_METHOD(createNewWindowWithResult:(BOOL)result lockIdentifier:(NSInteger)lockIdentifier)
 {
-  // if (createNewWindowCondition && [createNewWindowCondition tryLockWhenCondition:lockIdentifier]) {
-  //   createNewWindowResult = result;
-  //   [createNewWindowCondition unlockWithCondition:0];
-  // } else {
-  //   RCTLogWarn(@"createNewWindowWithResult invoked with invalid lockIdentifier: "
-  //              "got %zd, expected %zd", lockIdentifier, createNewWindowCondition.condition);
-  // }
+  if (createNewWindowCondition && [createNewWindowCondition tryLockWhenCondition:lockIdentifier]) {
+    createNewWindowResult = result;
+    [createNewWindowCondition unlockWithCondition:0];
+  } else {
+    RCTLogWarn(@"createNewWindowWithResult invoked with invalid lockIdentifier: "
+              "got %zd, expected %zd", lockIdentifier, createNewWindowCondition.condition);
+  }
 }
 
 RCT_REMAP_METHOD(addContentRuleList,
