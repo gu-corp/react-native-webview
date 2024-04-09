09c4205b111f6c65b19bb6ed2bd0161ce57b0e88
DaiHoang-Alobridge <greatbk1109@gmail.com>
2024-04-09 10:02:28 +0700

    Android: update Download Folder path for captureScreen() (#116)

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -699,6 +699,8 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
       if (downloadConfig.hasKey("downloadFolder")) {
         String downloadFolder = downloadConfig.getString("downloadFolder");
         RNCWebViewModule module = getModule((ReactContext) view.getContext());
+        RNCWebView rncWebView = (RNCWebView) view;
+        rncWebView.setDownloadFolder(downloadFolder);
         module.setDownloadFolder(downloadFolder);
         DOWNLOAD_FOLDER = downloadFolder;
       }
@@ -1753,8 +1755,8 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     private OnScrollDispatchHelper mOnScrollDispatchHelper;
     protected boolean hasScrollEvent = false;
     protected boolean nestedScrollEnabled = false;
-
     private static RNCWebView newWindow;
+    private String DOWNLOAD_FOLDER = "";
 
     /**
      * WebView must be created with an context of the current activity
@@ -1836,6 +1838,10 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
     public void setNestedScrollEnabled(boolean nestedScrollEnabled) {
       this.nestedScrollEnabled = nestedScrollEnabled;
+    }
+
+    public void setDownloadFolder(String downloadFolder) {
+      this.DOWNLOAD_FOLDER = downloadFolder;
     }
 
     @Override
@@ -2145,11 +2151,17 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
     public void captureScreen(String type) {
       final String fileName = System.currentTimeMillis() + ".jpg";
-      String directory = type.equals("SCREEN_SHOT") ? TEMP_DIRECTORY : DOWNLOAD_DIRECTORY;
+      // Old logic: save internal storage
+      // String directory = type.equals("SCREEN_SHOT") ? TEMP_DIRECTORY : DOWNLOAD_DIRECTORY;
 
-      File d = new File(directory);
-      d.mkdirs();
-      final String localFilePath = directory + fileName;
+      File saveDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+      if (DOWNLOAD_FOLDER != null && !DOWNLOAD_FOLDER.isEmpty()) {
+        saveDir = new File(saveDir, DOWNLOAD_FOLDER);
+        if (!saveDir.exists()) {
+          saveDir.mkdirs();
+        }
+      }
+      File downloadPath = new File(saveDir, fileName);
       boolean success = false;
       try {
         Picture picture = this.capturePicture();
@@ -2159,7 +2171,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         Canvas c = new Canvas(b);
         picture.draw(c);
 
-        FileOutputStream fos = new FileOutputStream(localFilePath);
+        FileOutputStream fos = new FileOutputStream(downloadPath, false);
         if (fos != null) {
           b.compress(Bitmap.CompressFormat.JPEG, 80, fos);
           fos.close();
@@ -2173,7 +2185,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         event.putBoolean("result", success);
         event.putString("type", type);
         if (success) {
-          event.putString("data", localFilePath);
+          event.putString("data", downloadPath.getAbsolutePath());
         }
         dispatchEvent(this, new TopCaptureScreenEvent(this.getId(), event));
       }
