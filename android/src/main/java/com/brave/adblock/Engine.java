ca9dc1d6fedc159fe0730e7f1f53527f08089042
greathoang <greatbk1109@gmail.com>
2024-04-19 13:28:03 +0700

    Android: update adblock v0.8.8

--- android/src/main/java/com/brave/adblock/Engine.java
+++ android/src/main/java/com/brave/adblock/Engine.java
@@ -9,7 +9,7 @@ public class Engine {
 
     private static native long engineCreate(String rules);
     private static native void engineDestroy(long engine);
-    private static native BlockerResult engineMatch(long engine, String url, String host, String tabHost, boolean thirdParty, String resourceType);
+    private static native BlockerResult engineMatch(long engine, String url, String sourceUrl, String resourceType);
 
     public Engine(String rules) {
         engine = engineCreate(rules);
@@ -20,7 +20,7 @@ public class Engine {
         engineDestroy(engine);
     }
 
-    public BlockerResult match(String url, String host, String tabHost, boolean thirdParty, String resourceType) {
-        return engineMatch(engine, url, host, tabHost, thirdParty, resourceType);
+    public BlockerResult match(String url, String sourceUrl, String resourceType) {
+        return engineMatch(engine, url, sourceUrl, resourceType);
     }
 }
