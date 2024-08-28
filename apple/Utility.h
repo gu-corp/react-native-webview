d712296fc13c6b19e919e33d9eef6fc0a8aa2d53
ngocdinhalobridge <ngoc.dinh@alobridge.com>
2024-08-28 14:34:03 +0700

    add some file swift

---
+++ apple/Utility.h
@@ -0,0 +1,32 @@
+#import <Foundation/Foundation.h>
+#import "DownloadHelper.h"
+
+@interface Utility : NSObject
++ (NSString *)getBase64Data:(NSString *)base64Data;
++ (NSString *)getMimeTypeFromBase64Data:(NSString *)base64Data;
++ (NSString *)getFileExtensionFromBase64Data:(NSString *)base64Data;
+
+@property (class, nonatomic, strong) NSDictionary *downloadConfig;
++ (NSURL *)getOrCreateFolderWithName:(NSString *)name excludeFromBackups:(BOOL)excludeFromBackups location:(NSSearchPathDirectory)location;
++ (NSURL *)uniqueDownloadPathForFilename:(NSString *)filename;
++ (NSURL *)getDownloadFolder;
++ (void)openDownloadFolder;
++ (NSArray *)getDownloadSessionInfo;
++ (void)setDownloadSessionInfo: (NSArray *)sessionInfos;
++ (void)initDownloadingList;
++ (void)removeSessionInfo: (NSNumber *)sessionId;
++ (void)setDownloadingInfos: (NSArray *)downloadInfos;
++ (NSNumber *)getNextIndexSessionInfo: (NSURLRequest *)url fileName: (NSString *)fileName mimeType: (NSString *)mimeType expectedFileSize: (NSNumber *)length;
++ (void) updateSessionInfo: (NSString *)sessionId downloadedSize: (NSNumber *)size status: (NSString *) status;
++ (NSString *)getSessionId: (NSURLSession *)session;
+
++ (NSNumber *)getLastSessionIndex;
++ (void)setLastSessionIndex: (NSNumber *)value;
+
+// AppInfo
++ (NSBundle *)applicationBundle;
++ (NSString *)bundleIdentifier;
++ (NSString *)appVersion;
++ (NSString *)buildNumber;
++ (NSString *)displayName;
+@end
